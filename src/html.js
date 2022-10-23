e151c89530b76e177c6ec9712fd55833f2becf1a
Morgan Kelly <mkelly5380@gmail.com>
2022-10-23 13:38:33 -0500

    created template literals for each different card in html

--- src/html.js
+++ src/html.js
@@ -1,3 +1,112 @@
+const generateHTML = (data) => {
+    const getManager = (manager) =>{
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
+
+    const getEngineer = (engineer) =>{
+        return ` 
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getName()}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGitHub()}" target="_blank" rel="noopener noreferrer">${engineer.getGitHub()}</a></li>
+            </ul>
+        </div>
+    </div> `;
+    };
+
+    const getIntern = (intern) => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+    `;
+    };
+
+let htmlCards = [];
 
 
-const generateHTML = (data) => {}
+htmlCards.push(
+    data
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => getManager(manager))
+  );
+  htmlCards.push(
+    data
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => getEngineer(engineer))
+      .join("")
+  );
+  htmlCards.push(
+    data
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => getIntern(intern))
+      .join("")
+  );
+
+  return html.join("");
+};
+
+modele.exports = (data) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+        <title>My Team</title>
+      </head>
+    
+      <body>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading bg-info">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+    
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                ${generateHTML(data)}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
+
+};

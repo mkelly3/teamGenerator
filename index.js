bb7dcdad9444e583097933b9f220c42bb01bda51
Morgan Kelly <mkelly5380@gmail.com>
2022-10-23 12:13:35 -0500

    added questions for the intern and engineer

--- index.js
+++ index.js
@@ -65,3 +65,67 @@ const addTeamMember = () => {
 }
 
 getManagerInfo();
+
+const addEngineer = () => {
+    return inquirer.prompt([
+    {
+        type:'input',
+        name: 'name',
+        message: "What is the Engineers' name?"
+    },
+    {
+        type:'input',
+        name:'Id',
+        message: "Enter Engineer's ID?"
+    },
+    {
+        type:'input',
+        name:'email',
+        message: "Enter engineer's email?"
+    },
+    {
+        type:'input',
+        name:'gitHubUser',
+        message: "Enter the engineer's gitHub username?"  
+    }
+
+    ])
+    .then((engineerResponse) =>{
+        const engineer = new Engineer(engineerResponse.name, engineerResponse.id,engineerResponse.email, engineerResponse.gitHubUser);
+        staffMembers.push(engineer);
+        console.log(staffMembers);
+    });
+}
+
+
+const addIntern = () => {
+    return inquirer.prompt([
+    {
+        type:'input',
+        name: 'name',
+        message: "What is the Interns' name?"
+    },
+    {
+        type:'input',
+        name:'Id',
+        message: "Enter Interns's ID?"
+    },
+    {
+        type:'input',
+        name:'email',
+        message: "Enter Interns's email?"
+    },
+    {
+        type:'input',
+        name:'school',
+        message: "Enter the Interns's school?"  
+    }
+
+    ])
+    .then((internAnswer) =>{
+        const intern = new Engineer(internAnswer.name, internAnswer.id,internAnswer.email, internAnswer.school);
+        staffMembers.push(intern);
+        console.log(staffMembers);
+    });
+}
+
